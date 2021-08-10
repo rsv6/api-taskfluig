25e2cc89863ae469b73f15ab7932dc3d79034e2b
rsv6 <rafaelsvbr@gmail.com>
2021-08-10 11:56:51 -0300

    add: fetch(post) teste

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -15,9 +15,17 @@ const get = async (req, res) => {
     return res.status(200).json(tasks)
   })
   .catch(err => console.log("Erro ao ler banco de dados", err))
-  
 }
 
+const post = async (req, res) => {
+  console.log("Route /teste via POST accessed")
+  const { fluig, descricao, status } = req.body
+
+  console.log(`Fluig: ${fluig} - Descricao: ${descricao} - Status: ${status}`)
+  return await res.json({ msg: `Fluig: ${fluig} - Descricao: ${descricao} - Status: ${status}`})
+}
+ 
 module.exports = {
   get,
+  post
 }
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -2,3 +2,4 @@
 module.exports = require('express')
   .Router()
     .get('/teste', require('../controllers/tasks').get)
+    .post('/teste', require('../controllers/tasks').post)
